eb8a6c6bb543e8ffffb28c2801bf823b8cba8c76
Justin Jacobs <jajdorkster@gmail.com>
2019-10-28 18:06:39 -0400

    Make the "Show Resolved" checkbox on the Vehicle Status page work

--- transit/views/vehicle_status.py
+++ transit/views/vehicle_status.py
@@ -6,6 +6,22 @@ def vehicleStatus(request):
     return render(request, 'vehicle/status/view.html', context={})
 
 def ajaxVehicleStatus(request):
-    vehicle_issues = VehicleIssue.objects.all()
-    return render(request, 'vehicle/status/ajax_view.html', {'vehicle_issues': vehicle_issues})
+    filter_show_resolved_str = request.GET.get('filter_show_resolved', None)
+
+    if filter_show_resolved_str is not None:
+        filter_show_resolved_str = filter_show_resolved_str.lower()
+        request.session['vehicle_status_filter_show_resolved'] = True if filter_show_resolved_str == "true" else False
+
+    filter_show_resolved = request.session.get('vehicle_status_filter_show_resolved', False)
+
+    if not filter_show_resolved:
+        vehicle_issues = VehicleIssue.objects.filter(is_resolved=False)
+    else:
+        vehicle_issues = VehicleIssue.objects.all()
+
+    context = {
+        'vehicle_issues': vehicle_issues,
+        'filter_show_resolved': filter_show_resolved
+    }
+    return render(request, 'vehicle/status/ajax_view.html', context=context)
 

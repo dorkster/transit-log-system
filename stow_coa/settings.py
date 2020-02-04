82f5d71ca16f38804d4a56606e0a0b00b439ad3e
Justin Jacobs <jajdorkster@gmail.com>
2020-02-04 16:51:28 -0500

    Define STATIC_ROOT in site settings

--- stow_coa/settings.py
+++ stow_coa/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/transit/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)

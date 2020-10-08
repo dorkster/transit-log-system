596beb22e2c7cf71af0e3ae669c59259ad2efa86
Justin Jacobs <jajdorkster@gmail.com>
2020-10-08 16:19:49 -0400

    Add missing models to admin.py

--- transit/admin.py
+++ transit/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from transit.models import Trip, Driver, Vehicle, TripType, Shift, Client, VehicleIssue, Template, TemplateTrip, ScheduleMessage, FrequentTag, PreTrip, SiteSettings
+from transit.models import Trip, Driver, Vehicle, TripType, Shift, Client, VehicleIssue, Template, TemplateTrip, ScheduleMessage, FrequentTag, PreTrip, Fare, Tag, SiteSettings
 
 # Register your models here.
 admin.site.register(Trip)
@@ -14,5 +14,7 @@ admin.site.register(TemplateTrip)
 admin.site.register(ScheduleMessage)
 admin.site.register(FrequentTag)
 admin.site.register(PreTrip)
+admin.site.register(Fare)
+admin.site.register(Tag)
 admin.site.register(SiteSettings)
 

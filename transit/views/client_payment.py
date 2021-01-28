2634d5bbf880f30574e771461c1691aec0a7248f
Justin Jacobs <jajdorkster@gmail.com>
2021-01-28 18:56:26 -0500

    Add sorting to Client Payments page

--- transit/views/client_payment.py
+++ transit/views/client_payment.py
@@ -85,6 +85,13 @@ def clientPaymentDelete(request, parent, id):
 
 @permission_required(['transit.view_clientpayment'])
 def ajaxClientPaymentList(request, parent):
+    SORT_DATE_PAID = 0
+    SORT_CASH = 1
+    SORT_CHECK = 2
+
+    sort_mode = request.session.get('client_payments_sort', SORT_DATE_PAID)
+    sort_mode_dir = request.session.get('client_payments_sort_dir', 0)
+
     request_id = ''
     if request.GET['target_id'] != '':
         request_id = uuid.UUID(request.GET['target_id'])
@@ -92,14 +99,34 @@ def ajaxClientPaymentList(request, parent):
     request_action = request.GET['target_action']
     request_data = request.GET['target_data']
 
-    # TODO column sort?
+    if request_action == 'sort':
+        new_sort_mode = int(request_data)
+        if sort_mode == new_sort_mode:
+            sort_mode_dir = 1 if sort_mode_dir == 0 else 0
+        else:
+            sort_mode_dir = 0
+        sort_mode = new_sort_mode
+        request.session['client_payments_sort'] = new_sort_mode
+        request.session['client_payments_sort_dir'] = sort_mode_dir
 
     client_payments = ClientPayment.objects.filter(parent=parent)
+
+    if sort_mode == SORT_DATE_PAID:
+        client_payments = client_payments.order_by('date_paid')
+    elif sort_mode == SORT_CASH:
+        client_payments = client_payments.order_by('cash', 'date_paid')
+    elif sort_mode == SORT_CHECK:
+        client_payments = client_payments.order_by('check', 'date_paid')
+
+    if sort_mode_dir == 1:
+        client_payments = client_payments.reverse()
 
     context = {
         'parent': parent,
         'client_payments': client_payments,
         'client': Client.objects.get(id=parent),
+        'sort_mode': sort_mode,
+        'sort_mode_dir': sort_mode_dir,
     }
     return render(request, 'client/payment/ajax_list.html', context=context)
 

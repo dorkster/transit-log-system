0a24f054f0b57baadadd10c40a636f5e54d5aea1
Justin Jacobs <jajdorkster@gmail.com>
2021-02-15 20:05:01 -0500

    Add sorting to event log

--- transit/views/loggedevent.py
+++ transit/views/loggedevent.py
@@ -21,6 +21,16 @@ def ajaxLoggedEventList(request):
     if not request.user.has_perm('transit.view_loggedevent'):
         return HttpResponseRedirect(reverse('login_redirect'))
 
+    SORT_TIMESTAMP = 0
+    SORT_USERNAME = 1
+    SORT_IP_ADDRESS = 2
+    SORT_ACTION = 3
+    SORT_TARGET = 4
+    SORT_DESCRIPTION = 5
+
+    sort_mode = request.session.get('eventlog_sort', SORT_TIMESTAMP)
+    sort_mode_dir = request.session.get('eventlog_sort_dir', 0)
+
     request_id = ''
     if request.GET['target_id'] != '':
         request_id = uuid.UUID(request.GET['target_id'])
@@ -44,6 +54,15 @@ def ajaxLoggedEventList(request):
     elif request_action == 'clear_log':
         for i in LoggedEvent.objects.all():
             i.delete()
+    elif request_action == 'sort':
+        new_sort_mode = int(request_data)
+        if sort_mode == new_sort_mode:
+            sort_mode_dir = 1 if sort_mode_dir == 0 else 0
+        else:
+            sort_mode_dir = 0
+        sort_mode = new_sort_mode
+        request.session['eventlog_sort'] = new_sort_mode
+        request.session['eventlog_sort_dir'] = sort_mode_dir
 
     filter_action = request.session.get('eventlog_filter_action', '')
     filter_target = request.session.get('eventlog_filter_target', '')
@@ -71,6 +90,22 @@ def ajaxLoggedEventList(request):
 
     logged_events = logged_events.order_by('-timestamp')
 
+    if sort_mode == SORT_TIMESTAMP:
+        logged_events = logged_events.order_by('-timestamp')
+    elif sort_mode == SORT_USERNAME:
+        logged_events = logged_events.order_by('username', '-timestamp')
+    elif sort_mode == SORT_IP_ADDRESS:
+        logged_events = logged_events.order_by('ip_address', '-timestamp')
+    elif sort_mode == SORT_ACTION:
+        logged_events = logged_events.order_by('event_action', '-timestamp')
+    elif sort_mode == SORT_TARGET:
+        logged_events = logged_events.order_by('event_model', '-timestamp')
+    elif sort_mode == SORT_DESCRIPTION:
+        logged_events = logged_events.order_by('event_desc', '-timestamp')
+
+    if sort_mode_dir == 1:
+        logged_events = logged_events.reverse()
+
     logged_event_pages = Paginator(logged_events, 25)
     logged_events_paginated = logged_event_pages.get_page(request.GET.get('page'))
 
@@ -95,6 +130,8 @@ def ajaxLoggedEventList(request):
         'actions': actions,
         'targets': targets,
         'current_ip': request.META['REMOTE_ADDR'],
+        'sort_mode': sort_mode,
+        'sort_mode_dir': sort_mode_dir,
     }
     return render(request, 'loggedevent/ajax_list.html', context=context)
 

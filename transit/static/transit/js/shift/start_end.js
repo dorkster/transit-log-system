09020d5c583878ac9f5d57f68843d6f2ff32efd1
Justin Jacobs <jajdorkster@gmail.com>
2022-08-25 23:35:29 -0400

    Shift/Trip: Attempt to provide better suggestions for mileage typos
    The following suggestion methods have been added:
    - Remove the first character
    - "Shift" the characters over if a decimal point was potentially missed
    
    But instead of providing multiple suggestions, we rank them by score and
    provide the best one. The score in this case is difference in mileage
    versus the reference mileage.
    
    Also defined now is an upper threshold for mileage. In other words,
    we'll offer suggestions if it looks like the mileage might be too high.
    This has been set to a slightly arbritrary 250 miles. The highest
    real-world mileage from existing data is around 80 miles. So the chances
    of a false positive here are low.
    
    Because there is now an upper threshold, we may end up with no
    reasonable suggestions. In such a case, we now simply display the option
    to clear the field and try again. I think this is a better option than
    presenting the user with a poor (and most likely inaccurate) suggestion.

--- transit/static/transit/js/shift/start_end.js
+++ transit/static/transit/js/shift/start_end.js
@@ -16,50 +16,110 @@ function showFullMiles(mile_data) {
     $("#full_miles").append('<span class="text-muted">' + prefix_miles + '</span><strong>' + new_miles + '</strong>');
 
     $("#mile_suggestion").hide();
-    if (shift_prev_miles != "" && combined_miles != "") {
-        if (parseFloat(combined_miles) < parseFloat(shift_prev_miles)) {
-            var prev_prefix_miles = shift_prev_miles.substring(0, shift_prev_miles.length - new_miles.length);
+    $("#mile_suggestion_error").hide();
+    $("#mile_suggestion_typo_warning").hide();
 
-            if ((combined_miles == prev_prefix_miles + new_miles) || (parseFloat(prev_prefix_miles + new_miles) < parseFloat(shift_prev_miles))) {
-                if (prev_prefix_miles == "") {
-                    prev_prefix_miles = "1";
-                }
-                else {
-                    prev_prefix_miles = (parseFloat(prev_prefix_miles) + 1).toString();
+    if (shift_prev_miles != "" && combined_miles != "") {
+        var high_mileage_threshold = 250;
+
+        var combined_miles_f = parseFloat(combined_miles);
+        var shift_prev_miles_f = parseFloat(shift_prev_miles);
+
+        if (combined_miles_f < shift_prev_miles_f || combined_miles_f >= shift_prev_miles_f + high_mileage_threshold) {
+            var suggestions = [];
+
+            var scored_suggestion = scoreSuggestion(combined_miles, shift_prev_miles, new_miles);
+            if (scored_suggestion[1] <= high_mileage_threshold) {
+                suggestions.push(scored_suggestion);
+            }
+
+            // try dropping the first character
+            var shortened_new_miles = new_miles;
+            if (shortened_new_miles.length > 3) {
+                shortened_new_miles = shortened_new_miles.substring(1);
+                var scored_suggestion = scoreSuggestion(combined_miles, shift_prev_miles, shortened_new_miles);
+                if (scored_suggestion[1] <= high_mileage_threshold) {
+                    suggestions.push(scored_suggestion);
                 }
             }
+
+            // try reformatting as if a decimal character was missed
+            if (new_miles.length >= 3 && new_miles.endsWith(".0")) {
+                var missed_decimal_miles = new_miles.slice(0, -3) + "." + new_miles.slice(-3, -2);
+                var scored_suggestion = scoreSuggestion(combined_miles, shift_prev_miles, missed_decimal_miles);
+                if (scored_suggestion[1] <= high_mileage_threshold) {
+                    suggestions.push(scored_suggestion);
+                }
+            }
+
+            // sort by score
+            suggestions.sort(function(a, b) {
+                return a[1] - b[1];
+            });
 
             var suggest_prefix = "";
             var suggest_suffix = "";
 
-            var suggest_miles = prev_prefix_miles + new_miles;
-            if (suggest_miles.length > combined_miles.length) {
-                suggest_suffix = suggest_miles;
+            $("#full_miles").append('<span class="text-danger"><strong><span class="ml-3 oi oi-warning"></span></strong></span>');
+            $("#mile_suggestion_typo_warning").show();
+
+            if (suggestions.length > 0) {
+                var suggest_miles = suggestions[0][0];
+                if (suggest_miles.length > combined_miles.length) {
+                    suggest_suffix = suggest_miles;
+                }
+                else {
+                    for (var i = 0; i < suggest_miles.length; i++) {
+                        if (suggest_miles.charAt(i) == combined_miles.charAt(i)) {
+                            suggest_prefix += suggest_miles.charAt(i);
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    suggest_suffix = suggest_miles.substring(suggest_prefix.length, suggest_miles.length);
+                }
+
+                $("#mile_suggestion").show();
+                $("#mile_suggestion_prefix").html(suggest_prefix);
+                $("#mile_suggestion_suffix").html(suggest_suffix);
+                $("#mile_suggestion_button").data("suggestion", suggest_suffix);
             }
             else {
-                for (var i = 0; i < suggest_miles.length; i++) {
-                    if (suggest_miles.charAt(i) == combined_miles.charAt(i)) {
-                        suggest_prefix += suggest_miles.charAt(i);
-                    }
-                    else {
-                        break;
-                    }
-                }
-                suggest_suffix = suggest_miles.substring(suggest_prefix.length, suggest_miles.length);
+                $("#mile_suggestion_error").show();
             }
-
-            $("#full_miles").append('<span class="text-danger"><strong><span class="ml-3 oi oi-warning"></span></strong></span>');
-            $("#mile_suggestion").show();
-            $("#mile_suggestion_prefix").html(suggest_prefix);
-            $("#mile_suggestion_suffix").html(suggest_suffix);
-            $("#mile_suggestion_button").data("suggestion", suggest_suffix);
         }
     }
+}
+
+function scoreSuggestion(combined_miles, shift_prev_miles, new_miles) {
+    var prev_prefix_miles = shift_prev_miles.substring(0, shift_prev_miles.length - new_miles.length);
+
+    if ((combined_miles == prev_prefix_miles + new_miles) || (parseFloat(prev_prefix_miles + new_miles) < parseFloat(shift_prev_miles))) {
+        if (prev_prefix_miles == "") {
+            prev_prefix_miles = "1";
+        }
+        else {
+            prev_prefix_miles = (parseFloat(prev_prefix_miles) + 1).toString();
+        }
+    }
+
+    var suggest_miles = prev_prefix_miles + new_miles;
+    var score = Math.abs(parseFloat(suggest_miles) - parseFloat(shift_prev_miles));
+
+    return [suggest_miles, score];
 }
 
 function useSuggestion(button, mile_data) {
     $("#id_miles").val($(button).data("suggestion"));
     showFullMiles(mile_data);
+    $("#id_miles").focus();
+}
+
+function mileageErrorTryAgain(mile_data) {
+    $("#id_miles").val("");
+    showFullMiles(mile_data);
+    $("#id_miles").focus();
 }
 
 function setupFormEvents(mile_data) {
